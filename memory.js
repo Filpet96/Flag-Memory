f4cdc032447c72ae2355d809a2e7bb395a1e5547
Filpet96 <Fpetersson96@gmail.com>
2017-12-11 19:22:11 +0100

    updated with gamesquare function

--- memory.js
+++ memory.js
@@ -1,3 +1,4 @@
+let areOpen = 0;
 // DOM element Reset button using ID to ALWAYS return a SINGLE element.
 let resetButton = document.getElementById("reset-button");
 
@@ -5,4 +6,15 @@ let resetButton = document.getElementById("reset-button");
 let flags = [];
 for (let i = 0; i < 10; i++) {
     flags.push('square-' + i);
+}
+// This is add clicks to the memory squares, and also to keep track of what state each memory square is.
+// The different states the squares can be in is open, closed, locked, unlocked.
+// Open or closed is if the squares default state is visible or if a user has clicked on it and the content is displayed.
+// Locked or unlocked means that either the square is locked and a user has matched it and then we want to ignore anymore clicks on that, unlocks ofcourse means the opposite ( no match).
+function GameSquare(el, flag){
+    this.el = el;
+    this.isOpen = false;
+    this.isLocked = false;
+    this.el.addEventListener("click", this, false);
+    this.setflag(flag); // Setting the flag.
 }
